46cdd1cb40206965856890aeb1505f3f20fe0972
cmchoi <cmchoi@fletacom.com>
2020-11-27 14:41:56 +0900

    batch commit recv add

--- ps_start.bat
+++ ps_start.bat
@@ -6,3 +6,10 @@ taskkill /f /IM pythonw.exe
 start cmd /c python ibrm_daemon_restapi.py
 start cmd /c python ibrm_server_daemon_socket.py
 start cmd /c python job_scheduler.py
+
+
+
+taskkill /f /IM fletaRecv.exe
+c:
+cd C:\Fleta\fleta_recv
+start cmd /c FletaRecv.exe
